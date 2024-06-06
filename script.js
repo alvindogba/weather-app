cb90f42c9d4191a1769a981de9c10c8268e39aad
unknown <alvindr2000@gmail.com>
2024-06-06 19:56:09 +0000

    Add Weather Icon

--- script.js
+++ script.js
@@ -3,15 +3,8 @@
 document.getElementById("userform").addEventListener("submit", (e)=>{
     e.preventDefault();
     let city = document.getElementById("userCity").value;
-    let responseH1= document.getElementById("h1");
-    // let currentDate= document.getElementById("currentDate")
-    let temValue=document.getElementById("temperatureValue")
-    let windSpeed=document.getElementById("windSpeed")
-    let humanidity=document.getElementById("humanidity")
-    // let image = document.getElementById("image")
     //git the current date
     // let date = new Date("month", "date", "year")
-    
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=13fb4c7688b3333de77e10b161e7d467`)
   .then(response => response.json())
@@ -19,7 +12,6 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=13fb4c768
     console.log(data);
      let latitute = data.coord.lat;
      let longitude = data.coord.lon; 
-
 
   //getting the weather data from the api
 return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitute}&lon=${longitude}&appid=13fb4c7688b3333de77e10b161e7d467`)
@@ -29,22 +21,26 @@ return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitute}&lo
   
  let viewName= data.name; //Name from the request
 let temperature=data.main.temp;//Temp from the api
-let wind = data.wind.speed;
+let windSpead = data.wind.speed;
 let hum = data.main.humidity;
-let image = data.weather[0].icon 
+let image = data.weather[0].icon; 
+let weatherDiscription= data.weather[0].description;
 
+let section =document.getElementById("container")
+ section.innerHTML=`<div class="card" id="view" >
+<h1 id="h1">${viewName}</h1> &nbsp; <span id="currentDate">2/23/2024</span>
+<h2>Temperature:  &nbsp; <span id="temperatureValue">${temperature}</span></h2>
+<h2>Wind:  &nbsp; <span id="windSpeed">${windSpead}</span></h2>
+<h2>Humidity:  &nbsp; <span id="humanidity">${ hum}</span></h2>
 
-temValue.textContent=temperature;
-responseH1.textContent=viewName;  //Assigning the name from the request to the view
-windSpeed.textContent=wind;
-humanidity.textContent=hum; 
-// image.src=icon_image;
+<div>
+  <img src="https://openweathermap.org/img/wn/${image}.png" alt="" id="image"> &nbsp <span>${weatherDiscription}</span>
+</div>
+</div>`
+
 })
-
-
 .catch(error => {
   console.log("Error fetching weather data:", error);
 })
   })
-
 })
