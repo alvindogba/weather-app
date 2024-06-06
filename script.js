a6c8f2c45f97bd94715add7a75fd5fd8580907b7
unknown <alvindr2000@gmail.com>
2024-06-06 21:39:18 +0000

    Add Style

--- script.js
+++ script.js
@@ -20,23 +20,28 @@ return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitute}&lo
     console.log(data)
   
  let viewName= data.name; //Name from the request
-let temperature=data.main.temp;//Temp from the api
+let temp=data.main.temp;//Temp from the api
+let temperatureCelsius =Math.floor(temp - 273.15) 
+console.log(temperatureCelsius)
 let windSpead = data.wind.speed;
 let hum = data.main.humidity;
 let image = data.weather[0].icon; 
 let weatherDiscription= data.weather[0].description;
 
-let section =document.getElementById("container")
- section.innerHTML=`<div class="card" id="view" >
-<h1 id="h1">${viewName}</h1> &nbsp; <span id="currentDate">2/23/2024</span>
-<h2>Temperature:  &nbsp; <span id="temperatureValue">${temperature}</span></h2>
-<h2>Wind:  &nbsp; <span id="windSpeed">${windSpead}</span></h2>
-<h2>Humidity:  &nbsp; <span id="humanidity">${ hum}</span></h2>
 
-<div>
-  <img src="https://openweathermap.org/img/wn/${image}.png" alt="" id="image"> &nbsp <span>${weatherDiscription}</span>
-</div>
-</div>`
+let section =document.getElementById("container")
+ section.innerHTML=`<div id="viewb" >
+                        <h1 id="h1">${viewName} &nbsp; <span id="currentDate">(2/34/2024)</h1></span>
+                        <h2>Temperature:  &nbsp; <span id="temperatureValue">${temperatureCelsius} &deg;C</span></h2>
+                        <h2>Wind:  &nbsp; <span id="windSpeed">${windSpead} M/S </span></h2>
+                        <h2>Humidity:  &nbsp; <span id="humanidity">${ hum}</span></h2>
+                        
+                    </div>
+                  
+                    <div class="img_container">
+                      <img src="https://openweathermap.org/img/wn/${image}.png" alt="" id="image"> <span>${weatherDiscription}</span>
+                    </div>`
+
 
 })
 .catch(error => {
